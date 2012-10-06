ffdb5bd03589173a32e7a0ad4d4613d639583284
Steve Pulec <spulec@gmail.com>
2012-10-06 11:57:18 -0400

    fix typeerror

--- ordrin/views.py
+++ ordrin/views.py
@@ -11,7 +11,7 @@ class AjaxSightings(JSONResponseMixin, View):
         if min_id:
             sightings = sightings.filter(id__gt=min_id)
             if sightings:
-                sightings = list(sightings)[-1]
+                sightings = [list(sightings)[-1]]
         else:
             sightings = sightings[:20]
         sightings = [x.__dict__ for x in sightings]
